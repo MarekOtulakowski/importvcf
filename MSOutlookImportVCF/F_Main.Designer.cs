383216209704205f5c2b7da19b677a27f5d5fb0b
marek.otulakowski@gmail.com <marek.otulakowski@gmail.com@84405dd2-3d18-4ffc-a9cd-0af8bda9a5d0>
2011-11-13 11:40:05 +0000

    v.1.0.0.0

---
+++ MSOutlookImportVCF/F_Main.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace MSOutlookImportVCF
+{
+    partial class F_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.B_browse = new System.Windows.Forms.Button();
+            this.L_pathInfo = new System.Windows.Forms.Label();
+            this.PB_progress = new System.Windows.Forms.ProgressBar();
+            this.TB_path = new System.Windows.Forms.TextBox();
+            this.B_import = new System.Windows.Forms.Button();
+            this.B_Abort = new System.Windows.Forms.Button();
+            this.L_stat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // B_browse
+            // 
+            this.B_browse.Location = new System.Drawing.Point(205, 29);
+            this.B_browse.Name = "B_browse";
+            this.B_browse.Size = new System.Drawing.Size(75, 23);
+            this.B_browse.TabIndex = 0;
+            this.B_browse.Text = "Browse";
+            this.B_browse.UseVisualStyleBackColor = true;
+            this.B_browse.Click += new System.EventHandler(this.B_browse_Click);
+            // 
+            // L_pathInfo
+            // 
+            this.L_pathInfo.AutoSize = true;
+            this.L_pathInfo.Location = new System.Drawing.Point(12, 15);
+            this.L_pathInfo.Name = "L_pathInfo";
+            this.L_pathInfo.Size = new System.Drawing.Size(85, 13);
+            this.L_pathInfo.TabIndex = 1;
+            this.L_pathInfo.Text = "Path to VCF files";
+            // 
+            // PB_progress
+            // 
+            this.PB_progress.Location = new System.Drawing.Point(12, 66);
+            this.PB_progress.Name = "PB_progress";
+            this.PB_progress.Size = new System.Drawing.Size(268, 11);
+            this.PB_progress.TabIndex = 2;
+            // 
+            // TB_path
+            // 
+            this.TB_path.Location = new System.Drawing.Point(12, 31);
+            this.TB_path.Name = "TB_path";
+            this.TB_path.Size = new System.Drawing.Size(186, 20);
+            this.TB_path.TabIndex = 3;
+            // 
+            // B_import
+            // 
+            this.B_import.Location = new System.Drawing.Point(49, 95);
+            this.B_import.Name = "B_import";
+            this.B_import.Size = new System.Drawing.Size(75, 23);
+            this.B_import.TabIndex = 4;
+            this.B_import.Text = "Import";
+            this.B_import.UseVisualStyleBackColor = true;
+            this.B_import.Click += new System.EventHandler(this.B_import_Click);
+            // 
+            // B_Abort
+            // 
+            this.B_Abort.Location = new System.Drawing.Point(163, 95);
+            this.B_Abort.Name = "B_Abort";
+            this.B_Abort.Size = new System.Drawing.Size(75, 23);
+            this.B_Abort.TabIndex = 5;
+            this.B_Abort.Text = "Abort";
+            this.B_Abort.UseVisualStyleBackColor = true;
+            this.B_Abort.Click += new System.EventHandler(this.B_abort_Click);
+            // 
+            // L_stat
+            // 
+            this.L_stat.AutoSize = true;
+            this.L_stat.Location = new System.Drawing.Point(12, 123);
+            this.L_stat.Name = "L_stat";
+            this.L_stat.Size = new System.Drawing.Size(53, 13);
+            this.L_stat.TabIndex = 6;
+            this.L_stat.Text = "No action";
+            // 
+            // F_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 144);
+            this.Controls.Add(this.L_stat);
+            this.Controls.Add(this.B_Abort);
+            this.Controls.Add(this.B_import);
+            this.Controls.Add(this.TB_path);
+            this.Controls.Add(this.PB_progress);
+            this.Controls.Add(this.L_pathInfo);
+            this.Controls.Add(this.B_browse);
+            this.Name = "F_Main";
+            this.Text = "Import VCF to MS Outlook v.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button B_browse;
+        private System.Windows.Forms.Label L_pathInfo;
+        private System.Windows.Forms.ProgressBar PB_progress;
+        private System.Windows.Forms.TextBox TB_path;
+        private System.Windows.Forms.Button B_import;
+        private System.Windows.Forms.Button B_Abort;
+        private System.Windows.Forms.Label L_stat;
+    }
+}
+
---
+++ MSOutlookImportVCF/F_Main.cs
@@ -0,0 +1,208 @@
+﻿#region UsesDirective
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Office;
+using Microsoft.Office.Core;
+using System.Runtime.InteropServices;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using System.Threading; 
+#endregion
+
+namespace MSOutlookImportVCF
+{
+    public partial class F_Main : Form
+    {
+        #region Zmienne
+        string pathToVcfFolder = string.Empty;
+        List<string> listPathsVcfFiles;
+        System.Windows.Forms.Timer timer;
+        float percentProgress = 0;
+        Thread theThread;
+        bool done = false; 
+        #endregion
+
+        #region Constructors
+        public F_Main()
+        {
+            InitializeComponent();
+        } 
+        #endregion
+
+        #region WithoutOutlook
+        private void B_browse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult dr = fbd.ShowDialog();
+
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                pathToVcfFolder = fbd.SelectedPath;
+                TB_path.Text = pathToVcfFolder;
+            }
+        }
+
+        private void B_import_Click(object sender, EventArgs e)
+        {
+            done = false;
+            PB_progress.Value = 0;
+            L_stat.Text = "No action";
+
+            //initial list
+            if (listPathsVcfFiles == null)
+            {
+                listPathsVcfFiles = new List<string>();
+            }
+            else
+            {
+                listPathsVcfFiles.Clear();
+            }
+
+            //initial timer
+            if (timer == null)
+            {
+                timer = new System.Windows.Forms.Timer();
+                timer.Interval = 1000;
+                timer.Tick += new EventHandler(timer_Tick);
+            }
+
+            //simple validate
+            if (!string.IsNullOrEmpty(TB_path.Text))
+            {
+                pathToVcfFolder = TB_path.Text;
+
+                try
+                {
+                    Get_listOfVcfFiles();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error during get files\n" + ex.Message,
+                                    "Error",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+                }
+
+                L_stat.Text = "Found " + 
+                              listPathsVcfFiles.Count.ToString() + 
+                              " files *.vcf";
+
+                theThread = new Thread(ImportContactsToMSOutlook);
+                theThread.Start();
+
+                timer.Start();
+            }
+            else
+            {
+                MessageBox.Show("Path to folder with VCF files is empthy!\nEnter correctly path and try again", 
+                                "Information", 
+                                MessageBoxButtons.OK, 
+                                MessageBoxIcon.Information);
+            }
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            PB_progress.Value = (int)percentProgress;
+
+            if (done)
+            {
+                L_stat.Text = "Imported " + 
+                              listPathsVcfFiles.Count + 
+                              " contacts, operation is complete!";
+            }
+        }
+
+        private void Get_listOfVcfFiles()
+        {
+            DirectoryInfo rootFolder = new DirectoryInfo(pathToVcfFolder);
+            FileInfo[] directoryFiles = rootFolder.GetFiles("*.vcf");
+
+            foreach (FileInfo file in directoryFiles)
+            {
+                listPathsVcfFiles.Add(file.FullName.ToLower());
+            }
+
+            if (rootFolder.GetDirectories().Length > 0)
+            {
+                foreach (DirectoryInfo directory in rootFolder.GetDirectories())
+                {
+                    Get_filesFromSubfolders(directory);
+                }
+            }
+        }
+
+        private void Get_filesFromSubfolders(DirectoryInfo _rootFolder)
+        {
+            FileInfo[] plikiFolderu = _rootFolder.GetFiles("*.vcf");
+
+            foreach (FileInfo file in plikiFolderu)
+            {
+                listPathsVcfFiles.Add(file.FullName.ToLower());
+            }
+
+            if (_rootFolder.GetDirectories().Length > 0)
+            {
+                foreach (DirectoryInfo directory in _rootFolder.GetDirectories())
+                {
+                    Get_filesFromSubfolders(directory);
+                }
+            }
+        }
+
+        private void B_abort_Click(object sender, EventArgs e)
+        {
+            if (theThread.IsAlive)
+            {
+                timer.Stop();
+                theThread.Abort();
+                MessageBox.Show("Abort for user wishes!",
+                                "Information",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+                L_stat.Text = "Abort for user wishes";
+            }
+        }
+        #endregion
+
+        #region OutlookFunction
+        private void ImportContactsToMSOutlook()
+        {
+            try
+            {
+                Outlook.Application oApp = new Outlook.Application();
+                Outlook.NameSpace oNS = oApp.GetNamespace("MAPI");
+
+                Outlook.MAPIFolder oContactsFolder = oNS.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderContacts) as Outlook.MAPIFolder;
+
+                int i = 0;
+                Outlook.ContactItem contact = null;
+                foreach (string oneContact in listPathsVcfFiles)
+                {
+                    i++;
+                    percentProgress = (float)i / listPathsVcfFiles.Count * 100;
+
+                    contact = oApp.Session.OpenSharedItem(oneContact) as Outlook.ContactItem;
+                    contact.Save();
+                    contact = null;
+                }
+
+                done = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error during adding new contacts to Outlook\n" + ex.Message,
+                                "Error",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+            }
+        } 
+        #endregion
+    }
+}
